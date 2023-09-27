b9aa2558df0a5440b7fd7760a4ce4e475a7d5ead
DongokYang <dyang2@academic.rrc.ca>
2023-09-27 14:11:44 -0500

    made atm interface in bank transactions.py

--- bank_transcations.py
+++ bank_transcations.py
@@ -1,6 +1,27 @@
 """
 Description:
-Author:
-Date:
+Author: Dongok Yang
+Date: 2023.09.27
 Usage:
 """
+import random
+
+
+transaction_options = ["D", "W", "Q"]
+current_balance = float(random.randint(-1000, 10000))
+current_balance = "${:,.2f}".format(current_balance)
+
+atm_interface = [
+    "***************************************",
+    "PIXELL RIVER FINANCIAL".center(40),
+    "ATM Simulator".center(40),
+    f"Your current balance is: {current_balance}".center(40),
+    "",
+    "Deposit: D".center(40),
+    "Withdraw: W".center(40),
+    "To Quit: Q".center(40),
+    "***************************************"
+]
+# Print the centered ATM interface
+for line in atm_interface:
+    print(line)
