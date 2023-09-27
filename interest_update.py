d666827544d925ce123edab8dc267fac9714510c
DongokYang <dyang2@academic.rrc.ca>
2023-09-27 18:10:58 -0500

    made a code which enables update account_balance.txt with interest applied one in iinterest_update.py

--- interest_update.py
+++ interest_update.py
@@ -16,5 +16,20 @@ with open("account_balances.txt","r") as file:
         account_number, account_balance = number_balance
         account_data[account_number] = float(account_balance)
 
-pp = pprint.PrettyPrinter(width=20)
+pp = pprint.PrettyPrinter(20)
 pp.pprint(account_data)
+
+for account_number,opening_balance in account_data.items():
+    print(opening_balance)
+    if opening_balance >= 5000:
+        closing_balance = opening_balance +((opening_balance *0.05)/12)
+    elif opening_balance >= 1000:
+        closing_balance = opening_balance +((opening_balance *0.025)/12)
+    elif opening_balance >=0:
+        closing_balance = opening_balance +((opening_balance *0.01)/12)
+    else:
+        closing_balance = opening_balance +((opening_balance *0.10)/12)
+    account_data[account_number] = f"{closing_balance:.6f}"
+
+pp.pprint(account_data)
+
